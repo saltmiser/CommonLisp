f9c8fc6648848852ad5f76e631a41fb395ef7e98
Jim Capozzoli <saltmiser@gmail.com>
2014-11-05 17:35:06 -0500

    Borrowed a hash-keys function from StackOverflow that is supposed to be runtime efficient.

--- UnweightedGraph.lisp
+++ UnweightedGraph.lisp
@@ -1,3 +1,8 @@
+;; The following function was deemed easy enough to write by anybody while
+;; still being run-time efficient.  
+;; http://stackoverflow.com/questions/9719852/common-lisp-getting-all-keys-of-a-given-hash-table-as-a-list
+(defun hash-keys (hash-table)
+  (loop for key being the hash-keys of hash-table collect key))
 
 (defclass node ()
   ((stored-value
@@ -6,12 +11,20 @@
     :accessor stored-value
     :documentation "The item to be stored at this node.")))
 )
+
 (defclass edge ()
-  ((node-list
+  ((connects-two-nodes node-list
     :initarg :node-list
     :initform (error "An edge connects two nodes.")
-    :accessor node-list
+    :accessor connects-two-nodes
     :documentation "Provide a list of length 2 containing the connecting nodes."
     )))
+
+(defclass graph ()
+  ((node-graph
+    :initform (make-hash-table)
+    :accessor node-list
+    :documentation "The hash table where nodes are the key and a list of edges are the value.")))
+
 
 
