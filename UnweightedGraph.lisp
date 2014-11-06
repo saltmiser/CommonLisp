c8023a8dfc03da3b86ce614f34d37e832883d429
Jim Capozzoli <saltmiser@gmail.com>
2014-11-05 20:06:46 -0500

    connect-nodes is almost working, I just have to remember the vertex-edge-map contains a list of unweighted-edge objects, not a singular...

--- UnweightedGraph.lisp
+++ UnweightedGraph.lisp
@@ -37,3 +37,39 @@
 (defgeneric add-vertex (g vertex-value))
 (defgeneric connect-nodes (g first-vertex second-vertex))
 (defgeneric traverse-graph (g start-vertex end-vertex function))
+
+(defmethod add-vertex ((g graph) vertex-value)
+  (let ((v-map (vertex-map g))
+	(ve-map (vertex-edge-map g)))
+    (setf (gethash vertex-value v-map)
+	  (make-instance 'vertex :stored-value vertex-value))
+    (setf (gethash (gethash vertex-value v-map) ve-map)
+	  (list))))
+
+(defmethod connect-nodes 
+    ((g graph) (first-vertex vertex) (second-vertex vertex))
+  (let ((v1 first-vertex)
+	(v2 second-vertex)
+	(edge-map (vertex-edge-map g)))
+    (let ((newedge 
+	   (make-instance 'unweighted-edge
+			  :node-list (list v1 v2)))
+	  (v1-edges (gethash v1 edge-map))
+	  (v2-edges (gethash v2 edge-map)))
+      (progn 
+	(if (and v1-edges (find v1 (connects-two-nodes v1-edges)))
+	    nil
+	    (setf v1-edges (append v1-edges (list newedge))))
+	(if (and v2-edges (find v2 (connects-two-nodes v2-edges)))
+	    nil
+	    (setf v2-edges (append v2-edges (list newedge))))
+	nil))))
+
+
+(setf myg (make-instance 'graph))
+(add-vertex myg 'HELLO)
+(add-vertex myg 'GOODBYE)
+(connect-nodes myg (gethash 'HELLO (vertex-map myg))
+	       (gethash 'GOODBYE (vertex-map myg)))
+
+
