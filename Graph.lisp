b551dd03fb28a8ab5e936017daec18997f1b06dc
James Capozzoli <saltmiser@gmail.com>
2014-11-24 06:32:36 -0500

    Preliminary generic definitions complete.

--- Graph.lisp
+++ Graph.lisp
@@ -22,7 +22,7 @@
   ;; track of edges and where they go.  Use an iedge (informed-edge) if this informaion is important to you.  
   ((weight
     :initarg :weight
-    :initform (error "You must provide a weight value for this to make sense.  Even if all weights are equal.")
+    :initform (error "You must provide a comparable value applicable to <, >, =, etc.")
     :accessor weight
     :documentation "The weight of this edge.")))
 
@@ -43,4 +43,21 @@
     :accessor symbol-vertex-table
     :documentation "A convenience table to store symbol/vertex (key/symbol) values.")))
 
-
+;; Follows is a list of generic method definitions which will mutate
+;; graph "grph" objects which are made up of vertex "vrtx" and "edge" objects.  
+;;
+;; Add new vertex to graph with stored-value
+(defgeneric add-vertex (grph vrtx-stored-value))
+;; Add new edge from first-vrtx to second-vrtx with edge-weight.
+(defgeneric connect (grph first-vrtx second-vrtx edge-weight))
+(defgeneric connect-by-key (grph 1-vrtx 2-vrtx edge-weight))
+;; Return a list of all neighbors from a vertex
+(defgeneric vertex-neighbors (grph vrtx))
+(defgeneric vertex-neighbors-by-symbol (grph vrtx)) ;; Convenience methods
+(defgeneric vertex-neighbors-by-symbol-return-symbols (grph vrtx))
+;; Traverse graph using shortest path and apply a function along the way.
+;; Returns the path length.
+(defgeneric graph-search-apply-fn (grph start-vrtx end-vrtx function)) 
+;; Traverse graph using shortest path and collect result of function
+;; along the way.  Returns said collected list.  
+(defgeneric graph-search-collect-fn (grph start-vrtx end-vrtx function))
